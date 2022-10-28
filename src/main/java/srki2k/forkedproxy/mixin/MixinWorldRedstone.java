3bf9345e8b182466c77fda4cd8bc9a93e13b5fa3
SrdjanV <SrdjanV@protonmail.com>
2022-10-28 14:24:16 +0200

    Moved redstone mixins to server

--- src/main/java/srki2k/forkedproxy/mixin/MixinWorldRedstone.java
+++ src/main/java/srki2k/forkedproxy/mixin/MixinWorldRedstone.java
@@ -1,26 +1,36 @@
 package srki2k.forkedproxy.mixin;
 
+import net.minecraft.profiler.Profiler;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.minecraft.world.WorldProvider;
-import org.spongepowered.asm.mixin.Final;
+import net.minecraft.world.WorldServer;
+import net.minecraft.world.storage.ISaveHandler;
+import net.minecraft.world.storage.WorldInfo;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import srki2k.forkedproxy.common.datamanagement.WorldProxyManager;
 import srki2k.forkedproxy.common.tileentity.TileAccessProxy;
 
-@Mixin(World.class)
-public abstract class MixinWorldRedstone {
+@SuppressWarnings("all")
+@Mixin(WorldServer.class)
+public abstract class MixinWorldRedstone extends World {
+    protected MixinWorldRedstone(ISaveHandler saveHandlerIn, WorldInfo info, WorldProvider providerIn, Profiler profilerIn, boolean client) {
+        super(saveHandlerIn, info, providerIn, profilerIn, client);
+    }
 
-    @Shadow
-    @Final
-    public WorldProvider provider;
+    @Unique(silent = true)
+    @Override
+    public int getRedstonePower(BlockPos pos, EnumFacing facing) {
+        return super.getRedstonePower(pos, facing);
+    }
 
-    @Inject(at = @At("RETURN"), method = "getRedstonePower(Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/util/EnumFacing;)I", cancellable = true)
+    @Inject(method = {"getRedstonePower(Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/util/EnumFacing;)I",
+            "func_175651_c(Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/util/EnumFacing;)I"}, at = @At("RETURN"),remap = false, cancellable = true)
     public void getRedstonePower(BlockPos pos, EnumFacing facing, CallbackInfoReturnable<Integer> callback) {
         TileAccessProxy proxy = WorldProxyManager.getRedstoneProxiesFromTarget(provider.getDimension(), pos.offset(facing.getOpposite()));
         if (proxy == null) {
@@ -30,7 +40,14 @@ public abstract class MixinWorldRedstone {
         callback.setReturnValue(Math.max(callback.getReturnValue(), proxy.getRedstonePowerForTarget()));
     }
 
-    @Inject(at = @At("RETURN"), method = "getStrongPower(Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/util/EnumFacing;)I", cancellable = true)
+    @Override
+    @Unique(silent = true)
+    public int getStrongPower(BlockPos pos, EnumFacing direction) {
+        return super.getStrongPower(pos, direction);
+    }
+
+    @Inject(method = {"getStrongPower(Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/util/EnumFacing;)I",
+            "func_175627_a(Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/util/EnumFacing;)I"}, at = @At("RETURN"),remap = false, cancellable = true)
     public void getStrongPower(BlockPos pos, EnumFacing facing, CallbackInfoReturnable<Integer> callback) {
         TileAccessProxy proxy = WorldProxyManager.getRedstoneProxiesFromTarget(provider.getDimension(), pos.offset(facing.getOpposite()));
         if (proxy == null) {
